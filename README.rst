ea2322c3ce41ecad66eeb550c5174b5f55781ba3
Alex Yang <alex.yang@nextthought.com>
2018-10-18 13:00:44 -0500

    initial check-in

---
+++ README.rst
@@ -0,0 +1,9 @@
+=============================
+ nti.app.contenttypes.calendar
+=============================
+
+.. image:: https://travis-ci.org/NextThought/nti.app.contenttypes.calendar.svg?branch=master
+    :target: https://travis-ci.org/NextThought/nti.app.contenttypes.calendar?branch=master
+
+.. image:: https://coveralls.io/repos/github/NextThought/nti.app.contenttypes.calendar/badge.svg?branch=master
+    :target: https://coveralls.io/github/NextThought/nti.app.contenttypes.calendar?branch=master
---
+++ src/nti/app/contenttypes/calendar/tests/__init__.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import zope.testing.cleanup
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
+
+from nti.testing.base import AbstractTestBase
+
+from nti.testing.layers import GCLayerMixin
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
+
+    set_up_packages = ('nti.app.contenttypes.calendar',)
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls):
+        pass
+
+    @classmethod
+    def testTearDown(cls):
+        pass
+
+
+class CalendarLayerTest(ApplicationLayerTest):
+
+    get_configuration_package = AbstractTestBase.get_configuration_package.__func__
+
+    set_up_packages = ('nti.app.contenttypes.calendar',)
