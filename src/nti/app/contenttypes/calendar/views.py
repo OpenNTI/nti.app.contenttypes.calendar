5d3300647dd765bae97ae5e42b02c0f2ed75fcf3
Alex Yang <alex.yang@nextthought.com>
2018-10-24 11:33:45 -0500

    change view permissions to match their actions.

--- src/nti/app/contenttypes/calendar/views.py
+++ src/nti/app/contenttypes/calendar/views.py
@@ -44,7 +44,7 @@ from nti.externalization.interfaces import StandardExternalFields
              renderer="rest",
              request_method='POST',
              context=ICalendar,
-             permission=nauth.ACT_UPDATE)
+             permission=nauth.ACT_CREATE)
 class CalendarEventCreationView(AbstractAuthenticatedView,
                                 ModeledContentUploadRequestUtilsMixin):
 
@@ -77,7 +77,7 @@ class CalendarEventUpdateView(UGDPutView):
              renderer='rest',
              context=ICalendarEvent,
              request_method='DELETE',
-             permission=nauth.ACT_UPDATE)
+             permission=nauth.ACT_DELETE)
 class CalendarEventDeletionView(AbstractAuthenticatedView):
 
     def __call__(self):
