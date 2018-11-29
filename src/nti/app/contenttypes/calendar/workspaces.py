0925c4d68f0738dfd53aefaba9e268f1d4fdec5e
Josh Zuech <josh.zuech@nextthought.com>
2018-11-29 16:20:22 -0600

    Add calendar collection view

--- src/nti/app/contenttypes/calendar/workspaces.py
+++ src/nti/app/contenttypes/calendar/workspaces.py
@@ -11,6 +11,8 @@ from __future__ import absolute_import
 from zope import component
 from zope import interface
 
+from zope.cachedescriptors.property import Lazy
+
 from zope.container.contained import Contained
 
 from nti.app.contenttypes.calendar import CALENDARS
@@ -19,6 +21,10 @@ from nti.app.contenttypes.calendar import EVENTS_VIEW_NAME
 from nti.app.contenttypes.calendar.interfaces import ICalendarCollection
 
 from nti.appserver.workspaces.interfaces import IUserWorkspace
+
+from nti.contenttypes.calendar.interfaces import ICalendarProvider
+
+from nti.externalization.interfaces import LocatedExternalList
 
 from nti.links.links import Link
 
@@ -41,12 +47,34 @@ class CalendarCollection(Contained):
     def user(self):
         return self.__parent__
 
+    @Lazy
+    def calendars(self):
+        """
+        Return a dict of course catalog entries the user is not enrolled
+        in and that are available to be enrolled in.
+        """
+        result = LocatedExternalList()
+        providers = component.subscribers((self.user,),
+                                          ICalendarProvider)
+        for x in providers or ():
+            result.extend(x.iter_calendars())
+        return result
+
+    @Lazy
+    def container(self):
+        container = self.calendars
+        container.__name__ = self.__name__
+        container.__parent__ = self.__parent__
+        container.lastModified = 0
+        return container
+
     @property
     def links(self):
         result = []
         result.append( Link(self.user,
                             rel=EVENTS_VIEW_NAME,
-                            elements=(self.__name__, '@@'+EVENTS_VIEW_NAME,),
+                            elements=(self.__name__,
+                                      '@@%s' % EVENTS_VIEW_NAME,),
                             method='GET'))
         return result
 
